1ba329c7aee7718baee879b7ac96bfdcc7a0f338
get.dipen@gmail.com <get.dipen@gmail.com>
2022-07-05 11:31:27 -0700

    Alternative way to downcase email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   # Before saving to database, downcase email.
   # self keyword is optional on right hand side
-  before_save { self.email = email.downcase }
+  before_save {
+    # self.email = email.downcase
+    email.downcase!
+  }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    uniqueness: true
 end
