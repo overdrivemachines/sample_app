5232e063225044ffb9f9cbd67bd752f23207fa22
get.dipen@gmail.com <get.dipen@gmail.com>
2022-07-05 23:38:35 -0700

    Retrieving gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+  # Methods here automatically available in any view
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user, size: 80)
+    gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    # According to documentation it is:
+    # gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    # debugger
+  end
 end
