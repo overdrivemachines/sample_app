edf67457f79a26446cd330937124b69ba497d8f7
get.dipen@gmail.com <get.dipen@gmail.com>
2022-07-06 12:28:21 -0700

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -18,4 +18,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select "div.field_with_errors"
   end
 
+
+  # When user submits valid information, confirm that a user was created
+  test "valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
 end
