0068e1e54767a0c36bfc0ebda7c426e1b78adfb7
get.dipen@gmail.com <get.dipen@gmail.com>
2022-07-06 22:17:06 -0700

    Include SessionsHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper # Make SessionsHelper available in all controllers
   def hello
     render html: "hello, world!"
   end
