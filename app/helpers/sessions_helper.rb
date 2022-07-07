5e386c34176a10182243e001d2ad16cabc3f779c
get.dipen@gmail.com <get.dipen@gmail.com>
2022-07-06 21:28:58 -0700

    Save user's id in a temp cookie

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+  def log_in(user)
+    # Section 8.2.1
+    # Places a temporary cookie on the user’s browser
+    # containing an encrypted version of the user’s id.
+    # Expires immediately when the browser is closed.
+    session[:user_id] = user.id
+  end
 end
