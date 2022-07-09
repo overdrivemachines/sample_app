7dabd8dbf1b010a55dca7a90a0aca12af728c996
get.dipen@gmail.com <get.dipen@gmail.com>
2022-07-08 18:46:23 -0700

    Testing layout changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
 
     # if user exists and the password is correct
-    if user && user.authenticate(params[:session][:password])
+    # if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       # Reset Session to prevent Session Fixation
       reset_session
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   # We need to test the following:
   # Visit the login path.
   # Verify that the new sessions form renders properly.
@@ -16,6 +20,34 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template 'sessions/new'
     post login_path, params: { session: { email: "", password: "" } }
     assert_response :unprocessable_entity
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
+
+  # Visit the login path.
+  # Post valid information to the sessions path.
+  # Verify that the login link disappears.
+  # Verify that a logout link appears
+  # Verify that a profile link appears.
+  test "login with valid information" do
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
+  test "login with valid email/invalid password" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email:    @user.email,
+                                          password: "invalid" } }
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
