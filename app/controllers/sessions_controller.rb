6580cf28c920ffd9cc246e6328d4a6403d0fc58e
get.dipen@gmail.com <get.dipen@gmail.com>
2022-07-13 16:09:07 -0700

    Check if user is activated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,31 +10,39 @@ class SessionsController < ApplicationController
     # if user exists and the password is correct
     # if user&.authenticate(params[:session][:password])
     if @user && @user.authenticate(params[:session][:password])
-      # Log the user in and redirect
+      # User exists and the password provided is correct
 
-      forwarding_url = session[:forwarding_url]
+      if @user.activated?
+        # user is activated so we can log the user in and redirect
+        forwarding_url = session[:forwarding_url]
 
-      # Reset Session to prevent Session Fixation
-      reset_session
+        # Reset Session to prevent Session Fixation
+        reset_session
 
-      # check if remember me checkbox is set in the login form
-      if params[:session][:remember_me] == '1'
-        # remember method is defined in sessions_helper.rb. It calls
-        # user.remember and saves permanent cookies for:
-        # user_id (encrypted) and remember_token
-        remember(@user)
+        # check if remember me checkbox is set in the login form
+        if params[:session][:remember_me] == '1'
+          # remember method is defined in sessions_helper.rb. It calls
+          # user.remember and saves permanent cookies for:
+          # user_id (encrypted) and remember_token
+          remember(@user)
+        else
+          forget(@user)
+        end
+
+        # temporary cookie containing user's id
+        # log_in() is defined in sessions_helper.rb
+        log_in @user
+
+        # redirect the user back to where they were or
+        # to the user's show page (eg: /users/1)
+        redirect_to forwarding_url || @user
       else
-        forget(@user)
+        # user is not activated so we cannot log the user in
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
       end
-
-      # temporary cookie containing user's id
-      # log_in() is defined in sessions_helper.rb
-      log_in @user
-
-      # redirect the user back to where they were or
-      # to the user's show page (eg: /users/1)
-      redirect_to forwarding_url || @user
-
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination'
