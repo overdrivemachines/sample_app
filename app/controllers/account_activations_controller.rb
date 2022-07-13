7f4befabad38451fe28be319c218e1687d985882
get.dipen@gmail.com <get.dipen@gmail.com>
2022-07-13 12:55:56 -0700

    Edit action gets called when user clicks on activation link in email

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,23 @@
 class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by(email: params[:email])
+    activation_token = params[:id]
+
+    # If: 1) user exists 2) user is activated and 3) user can be authenticated with the
+    # given activation token, then activate and log in the user
+    if user && !user.activated? && user.authenticated?(:activation, activation_token)
+      # Activate User
+      user.update_attribute(:activated, true)
+      user.update_attribute(:activated_at, Time.zone.now)
+
+      # Login User
+      # reset_session
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
+  end
 end
