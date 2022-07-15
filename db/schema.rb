1b583eef023ff2534f99d83abfac10cab0919b10
get.dipen@gmail.com <get.dipen@gmail.com>
2022-07-15 02:04:09 -0700

    Added reset_digest and reset_sent_at columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_13_073942) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_15_090335) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_073942) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
