cd63d6147905de7c00637890ee879d1896def3f6
get.dipen@gmail.com <get.dipen@gmail.com>
2022-07-16 23:48:58 -0700

    Debugging valid_user

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -57,20 +57,19 @@ class PasswordResetsController < ApplicationController
 
   # Confirms a valid user.
   def valid_user
+    if @user == nil
+      flash[:danger] = "User is not found"
+    else
+      if !@user.activated?
+        flash[:danger] = "User is not activated"
+      end
+      if !@user.authenticated?(:reset, params[:id])
+        flash[:danger] = "Remember token is not valid. Reset Digest: #{@user.reset_digest}"
+      end
+    end
     unless (@user &&
             @user.authenticated?(:reset, params[:id]) &&
             @user.activated?)
-      if @user == nil
-        flash[:danger] = "User is not found"
-      else
-        if !@user.activated?
-          flash[:danger] = "User is not activated"
-        end
-        if @user.authenticated?(:reset, params[:id])
-          flash[:danger] = "Remember token is not valid"
-        end
-      end
-
 
 
       redirect_to root_url
