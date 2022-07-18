b12ca3a8b2ba36cccc12a412e18376e8b7c4f49b
get.dipen@gmail.com <get.dipen@gmail.com>
2022-07-17 18:41:51 -0700

    Prevent userless microposts from being stranded in the database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
-
+  # Prevent userless microposts from being stranded in the database
+  has_many :microposts, dependent: :destroy
   before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
