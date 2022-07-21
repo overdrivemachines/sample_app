eb6131be431308115fd7607151732e0fb767b46c
get.dipen@gmail.com <get.dipen@gmail.com>
2022-07-20 17:46:18 -0700

    adding delete

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   # /microposts
   def create
@@ -15,15 +16,25 @@ class MicropostsController < ApplicationController
 
   # /microposts/1
   def destroy
-    @micropost = Micropost.find(params[:id])
     @micropost.destroy
-    redirect_to root_url
+    flash[:success] = "Micropost deleted"
+    # redirect back to the referring page because
+    # users can delete a micropost either from the home page or
+    # the user's profile page. If the referring url is nil,
+    # redirect to root url
+    redirect_back_or_to(root_url, status: :see_other)
   end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  # checks that the current user actually has a micropost with the given id
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url, status: :see_other if @micropost.nil?
   end
 
 end
